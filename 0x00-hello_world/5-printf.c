6f2c715a1d1bbd61b249f96b3b7eb388f8f0a20c
M-kip <bettmoseskiprono@gmail.com>
2022-03-10 15:47:44 -0800

    Adding task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * Program that prints to the screen
+ * using printf function
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
