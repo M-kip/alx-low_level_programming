56de30f48be477c07b13871509dc786e03a0dbfb
M-kip <bettmoseskiprono@gmail.com>
2022-03-10 16:11:00 -0800

    Hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,14 @@
  * Program that prints to the screen
  * using printf function
  */
-
+/**
+ * main - print to screen
+ *
+ * Description: Print to screen using printf function
+ * Return: 0 if successfull else 1
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
