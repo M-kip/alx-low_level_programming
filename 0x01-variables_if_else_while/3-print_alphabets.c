cec0273ef4e80fb13141a9910f7ce025f8c3d0e1
M-kip <bettmoseskiprono@gmail.com>
2022-03-15 09:32:21 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+/*
+ * Program to print the alphabet
+ * using putchar function
+ * Author: Moses Kiprono
+ * Email: bettmoseskiprono@gmail.com
+ */
+
+#include <stdio.h>
+
+/**
+ * main - print the alphabet
+ *
+ * Description: prints the alphabet using
+ * putchar function
+ * Return: 0 if successfull else 1
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 97; a <= 122; a++)
+		putchar(a);
+	for (b = 65; b <= 90; b++)
+		putchar(b);
+	putchar('\n');
+	
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -14,3 +14,4 @@
 | 0-positive_or_negative | Print a random number to the screen detailing if it's positive or negative|
 | 1-last_digit.c | Print the last digit of a number using mod|
 | 2-print_alphabet.c | Print the alphabet using putchar|
+| 3-print_alphabets.c | Prints lowercase and uppercase alphabets |
