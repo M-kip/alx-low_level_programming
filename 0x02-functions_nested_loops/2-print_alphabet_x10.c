7fcb17d83e1b880111370db1120a74b407dad4e0
M-kip <bettmoseskiprono@gmail.com>
2022-03-15 14:24:45 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+/*
+ * Program print the alphabet
+ * Author Moses Kiprono
+ *
+ */
+
+#include "main.h"
+/**
+ * print_alphabet - Program prints alphabet
+ *
+ *
+ * Return: on error -1 else Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for ( b = 0; b <= 10; b++)
+	{
+		for (a = 97; a <= 122; a++)
+			_putchar(a);
+		_putchar('\n');
+	}
+}
