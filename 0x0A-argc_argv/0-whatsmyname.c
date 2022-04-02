f32693db812ffc862db792799ea2615722410134
M-kip <bettmoseskiprono@gmail.com>
2022-04-01 18:14:21 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * Program to print it's name
+ * Author: Moses kiprono
+ */
+
+/**
+ * main - Print program name
+ * @argc: argument counter
+ * @argcv: argument vector
+ *
+ * return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,13 @@
+# Project argc, argv
+
+ **Technologies**
+-c
+-make
+
+ **files**
+------
+
+|file|description|
+|----|-----------|
+| 0-whatsmyname | prints out program name |
+
