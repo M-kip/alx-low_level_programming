4a435805ec7f752223c0cf2601e7459b3569928e
M-kip <bettmoseskiprono@gmail.com>
2022-04-22 02:55:47 +0300

    size of list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * list_len - print the lenght of a list
+ * @h: singly list
+ *
+ * Return: lenght int
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
