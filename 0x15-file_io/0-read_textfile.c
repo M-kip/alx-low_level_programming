9ce4413d360cf466dc7a32089758017c416af2a0
M-kip <bettmoseskiprono@gmail.com>
2022-05-06 20:13:45 +0300

    hot fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,19 +13,21 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nm = 0;
-	static char buf[BUFSIZE];
-	int n;
+	char *buf;
+	ssize_t n, m, nm = 0;
 
+	buf = malloc(letters * sizeof(char));
+	if (buf == NULL)
+		return (0);
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY, 0);
 	if (fd == -1)
 		return (0);
-	while ((n = read(fd, buf, letters)) > 0)
+	while ((m = read(fd, buf, letters)) > 0)
 	{
-		n = write(STDOUT_FILENO, buf, letters);
-		if (n != (int) letters || n == -1)
+		n = write(STDOUT_FILENO, buf, m);
+		if (n != m || n == -1)
 			return (0);
 		nm += n;
 	}
