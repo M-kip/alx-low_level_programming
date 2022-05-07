4d6ec20ad4c23d01abb3c07db065aae7093ab05f
M-kip <bettmoseskiprono@gmail.com>
2022-05-07 05:56:05 +0300

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,6 +34,6 @@ int create_file(const char *filename, char *text_content)
 		if (n != len || n == -1)
 			return (-1);
 	}
-
+	close(fd);
 	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,12 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <sys/file.h>
 #define BUFSIZE 1024
+#define PERMS 0600
 ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
 
 #endif
