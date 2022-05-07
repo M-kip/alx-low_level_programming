9b47d12ce2d3c65d49e8a39b90d0f0eb90d2867d
M-kip <bettmoseskiprono@gmail.com>
2022-05-07 06:03:18 +0300

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,6 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		for (len = 0; text_content[len]; len++)
 			;
+	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == 1)
 	{
@@ -38,7 +39,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			close(fd);
 			return (-1);
 		}
-
+	}
 	else if (fd == -1)
 		return (-1);
 
