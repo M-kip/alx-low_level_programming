03ce6e4eae39e03eef30fa9560ad55dd1c5138bc
Moses Kiprono <bettmoseskiprono@gmail.com>
2022-08-19 16:27:49 +0300

    quick fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -31,20 +31,24 @@ def island_perimeter(grid):
             down = 0
             val = grid[x][y]
 
-            if not ((y+1) > ncolumns):
+            if not ((y+1) >= ncolumns):
                 next_ = grid[x][y+1]
-            if not ((y-1) < 0):
+            if not ((y-1) <= 0):
                 prev = grid[x][y-1]
-            if not ((x-1) < nrows):
+            if not ((x-1) <= 0):
                 up = grid[x-1][y]
-            if not ((x+1) > nrows):
+            if not ((x+1) >= nrows):
                 down = grid[x+1][y]
 
             if val:
-                if prev == 0 or next_ == 0:
+                if prev or next_:
                     cells += 1
-                elif up == 0 or down == 0:
+                elif up or down:
                     cells += 1
                 else:
                     cells += 1
-    return cells
+    if cells:
+        if cells == 1:
+            return cells * 4
+        elif cells > 1:
+            return (cells * 2) + 2
